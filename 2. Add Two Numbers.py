4e62e97d6b98ea67442be41e953b86a89647845d
kshah23 <kshah23@ncsu.edu>
2024-08-28 21:18:50 -0400

    New method for 2.

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -7,35 +7,32 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
         curr1 = l1
-        curr2 = l2
-
-        head = ListNode(-1)
-        result = head
+        curr2, head2 = l2, l2
         carry = 0
+        prev1, prev2 = None, None
+
         while curr1 and curr2:
-            total_sum = curr1.val + curr2.val + carry
-            result.next = ListNode(total_sum%10)
-            carry = total_sum // 10
+            su = curr1.val + curr2.val + carry
+            carry = su//10
+            remainder = su%10
 
-            curr1 = curr1.next
-            curr2 = curr2.next
-            result = result.next
+            curr2.val = remainder
+            prev1, prev2 = curr1, curr2
+            curr1, curr2 = curr1.next, curr2.next
         
-        while curr1:
-            total_sum = curr1.val + carry
-            result.next = ListNode(total_sum%10)
-            carry = total_sum//10
-            curr1 = curr1.next
-            result = result.next
-        
-        while curr2:
-            total_sum = curr2.val + carry
-            result.next = ListNode(total_sum%10)
-            carry = total_sum//10
-            curr2 = curr2.next
-            result = result.next
+        prev = prev2 if prev2 else None
+        if curr1 or curr2:
+            if curr1:
+                prev2.next = curr1
+                curr2 = prev2.next
+            while curr2:
+                su = curr2.val + carry
+                carry = su//10
+                remainder = su%10
 
+                curr2.val = remainder
+                prev, curr2 = curr2, curr2.next
+        
         if carry:
-            result.next = ListNode(carry)
-
-        return head.next
+            prev.next = ListNode(1)
+        return head2
